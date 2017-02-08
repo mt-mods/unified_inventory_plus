83d32956bf0f668cf177fcc7afc6c77bc27d8c0c
bousket <bousket@gmail.com>
2017-02-08 15:47:32 +0100

    change default patterns order

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ local worldpath = minetest.get_worldpath()
 
 unified_inventory_plus = {
 
-	-- Patterns: (Comment unwanted ones & reorder on your wish: buttons are placed left to right, then up each 6)
+	-- Patterns: (Comment unwanted ones & reorder as you wish: buttons are then placed left to right, then up, 6 on a row)
 	-- pattern field contains indexes to fill in the craft inventory according to the scheme: (then others for non creatives)
 	--
 	-- 1 2 3
@@ -14,15 +14,15 @@ unified_inventory_plus = {
 	--
 	craft_patterns = {
 	{ico="pattern_1.png" , pattern={1}},
-	{ico="pattern_9.png" , pattern={1,2,3,4,5,6,7,8,9}},
 	{ico="pattern_4.png" , pattern={1,2,4,5}},
-	{ico="pattern_8.png" , pattern={1,2,3,4,6,7,8,9}},
-	{ico="pattern_5.png" , pattern={1,3,5,7,9}},
-	{ico="pattern_7.png" , pattern={1,2,3,4,5,6,8}},
+	{ico="pattern_9.png" , pattern={1,2,3,4,5,6,7,8,9}},
 	{ico="pattern_3.png" , pattern={1,2,3}},
 	{ico="pattern_3b.png", pattern={1,4,7}},
+	{ico="pattern_5.png" , pattern={1,3,5,7,9}},
 	{ico="pattern_6.png" , pattern={1,4,5,7,8,9}},
 	{ico="pattern_6b.png", pattern={1,2,3,4,5,6}},
+	{ico="pattern_7.png" , pattern={1,2,3,4,5,6,8}},
+	{ico="pattern_8.png" , pattern={1,2,3,4,6,7,8,9}},
 	},
 	
 	
